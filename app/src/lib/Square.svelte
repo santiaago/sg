d4bb8d998081dc88ab2b4df0087166e2a347d529
Santiago Arias <sas@lakera.ai>
2022-10-01 16:12:12 +0200

    2 svg drawing methods

---
+++ app/src/lib/Square.svelte
@@ -0,0 +1,16 @@
+<script>
+  const ns = "http://www.w3.org/2000/svg";
+  let svg = document.createElementNS(ns, "svg");
+  svg.setAttributeNS(null, "width", "100%");
+  svg.setAttributeNS(null, "height", "100%");
+  $: if (svg) draw(svg);
+  function draw(svg) {
+    const rect = document.createElementNS(ns, "rect");
+    rect.setAttributeNS(null, "width", "100");
+    rect.setAttributeNS(null, "height", "100");
+    rect.setAttributeNS(null, "fill", "#f06");
+    svg.appendChild(rect);
+  }
+</script>
+
+<svg bind:this={svg} />
---
+++ app/src/lib/Square2.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { SVG } from "@svgdotjs/svg.js";
+  import { onMount } from "svelte";
+  let svg;
+
+  onMount(() => {
+    let draw = SVG().addTo(svg);
+    const rect = draw.rect(100, 100).fill("#f0A");
+  });
+</script>
+
+<svg bind:this={svg} />
