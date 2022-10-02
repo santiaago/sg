e1818ad2fc71d7becf1ab448bd2c775a33df9607
Santiago Arias <sas@lakera.ai>
2022-10-02 12:51:02 +0200

    circles

--- app/src/lib/Sheet.svelte
+++ app/src/lib/Sheet.svelte
@@ -9,6 +9,23 @@
   onMount(() => {
     let draw = SVG().size("100%", "100%").viewbox(0, 0, 800, 1000).addTo(svg);
     const rect = draw.rect(width, height).fill("#fff");
+    const border = 50;
+    const line = draw
+      .line(border, height - border, width - border, height - border)
+      .stroke({ color: "#f06", width: 10, linecap: "round" });
+
+    const circle1 = draw
+      .circle()
+      .fill("none")
+      .stroke({ color: "#f06", width: 10 })
+      .radius(310)
+      .center(width - border, height - border);
+    const circle2 = draw
+      .circle()
+      .fill("none")
+      .stroke({ color: "#f06", width: 10 })
+      .radius(310)
+      .center(border, height - border);
   });
 </script>
 
