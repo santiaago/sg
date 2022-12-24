8ba84538f188c59941b10126ab67df4f0eefcced
Santiago Arias <sas@lakera.ai>
2022-12-24 15:27:08 +0100

    move funcs up

--- app/src/lib/SixFold.svelte
+++ app/src/lib/SixFold.svelte
@@ -236,6 +236,38 @@
     return [pi2x, pi2y];
   };
 
+  const isPointCloseToCircleBorder = (mouseEvent, cx, cy, r) => {
+    const [px, py] = d3.pointer(mouseEvent);
+    const dist = distance(px, py, cx, cy);
+    // epsilons for distance in and out of circle border
+    const eOut = 0.1;
+    const eIn = 1.4;
+    return (
+      (r - eOut) * (r - eOut) <= dist * dist &&
+      dist * dist <= (r + eIn) * (r + eIn)
+    );
+  };
+
+  const addCircleTooltipEvents = (selection, tooltip, cx, cy, d1, stroke) => {
+    selection
+      .on("mouseover", (d) => {
+        if (isPointCloseToCircleBorder(d, cx, cy, d1)) {
+          tooltip.map((x) => x.style("opacity", 1));
+          d3.select(d.currentTarget).style("stroke-width", strokeMid);
+        }
+      })
+      .on("mouseleave", (d) => {
+        if (isPointCloseToCircleBorder(d, cx, cy, d1)) {
+        } else {
+          tooltip.map((x) => x.style("opacity", 0));
+          d3.select(d.currentTarget)
+            .style("fill", "transparent")
+            .style("opacity", 1)
+            .style("stroke-width", stroke);
+        }
+      });
+  };
+
   onMount(() => {
     let svg = d3.select(el).attr("viewBox", "0 0 300 150");
 
@@ -303,38 +335,6 @@
     );
     // measure distance of intersection points
     const d1 = distance(pic14x, pic14y, pi2x, pi2y);
-
-    const isPointCloseToCircleBorder = (mouseEvent, cx, cy, r) => {
-      const [px, py] = d3.pointer(mouseEvent);
-      const dist = distance(px, py, cx, cy);
-      // epsilons for distance in and out of circle border
-      const eOut = 0.1;
-      const eIn = 1.4;
-      return (
-        (r - eOut) * (r - eOut) <= dist * dist &&
-        dist * dist <= (r + eIn) * (r + eIn)
-      );
-    };
-
-    const addCircleTooltipEvents = (selection, tooltip, cx, cy, d1, stroke) => {
-      selection
-        .on("mouseover", (d) => {
-          if (isPointCloseToCircleBorder(d, cx, cy, d1)) {
-            tooltip.map((x) => x.style("opacity", 1));
-            d3.select(d.currentTarget).style("stroke-width", strokeMid);
-          }
-        })
-        .on("mouseleave", (d) => {
-          if (isPointCloseToCircleBorder(d, cx, cy, d1)) {
-          } else {
-            tooltip.map((x) => x.style("opacity", 0));
-            d3.select(d.currentTarget)
-              .style("fill", "transparent")
-              .style("opacity", 1)
-              .style("stroke-width", stroke);
-          }
-        });
-    };
 
     circles.forEach(([cx, cy], i) => {
       const tooltip = text(svg, cx, cy, `c${i}-d1`);
